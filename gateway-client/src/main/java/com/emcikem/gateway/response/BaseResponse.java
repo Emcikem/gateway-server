1d0c56bd8f4a3a60e9d63ad0ced0fadc90c2961b
Emcikem <18968479985@163.com>
2022-07-16 15:17:34 +0800

    初始化common

--- gateway-client/src/main/java/com/emcikem/gateway/response/BaseResponse.java
+++ gateway-client/src/main/java/com/emcikem/gateway/response/BaseResponse.java
@@ -1,9 +1,35 @@
 package com.emcikem.gateway.response;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
 /**
  * @author Emcikem
  * @create 2022/7/16
- * @desc
  */
-public class BaseResponse {
+@NoArgsConstructor
+@AllArgsConstructor
+public class BaseResponse implements Serializable {
+
+    private int code;
+
+    private String message;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
--- gateway-common/src/main/java/com/emcikem/gateway/util/GsonUtil.java
+++ gateway-common/src/main/java/com/emcikem/gateway/util/GsonUtil.java
@@ -1,9 +1,64 @@
 package com.emcikem.gateway.util;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.internal.bind.DateTypeAdapter;
+import com.google.gson.reflect.TypeToken;
+import org.apache.commons.lang.StringUtils;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author Emcikem
  * @create 2022/7/16
- * @desc
  */
 public class GsonUtil {
+
+    private final static Gson GSON = (new GsonBuilder()).registerTypeAdapter(Date.class, new DateTypeAdapter()).create();
+
+    public static String toJsonString(Object object) {
+        return object == null ? "" : GSON.toJson(object);
+    }
+
+    public static <T> T parseObject(String json, Class<T> clz) {
+        return StringUtils.isBlank(json) ? null : GSON.fromJson(json, clz);
+    }
+
+    public static <T> List<T> parseList(String json, final Class<T> clazz) {
+        if (StringUtils.isBlank(json)) {
+            return Collections.emptyList();
+        } else {
+            Type type = new ParameterizedType() {
+                @Override
+                public Type[] getActualTypeArguments() {
+                    return new Type[]{clazz};
+                }
+                @Override
+                public Type getRawType() {
+                    return List.class;
+                }
+                @Override
+                public Type getOwnerType() {
+                    return null;
+                }
+            };
+            return GSON.fromJson(json, type);
+        }
+    }
+
+    public static <K, V> Map<K, V> toMap(String json, Type type) {
+        return GSON.fromJson(json, type);
+    }
+
+    public static <T> Map<String, T> gsonToMaps(String gsonString) {
+        Map<String, T> map = null;
+        map = GSON.fromJson(gsonString, new TypeToken<Map<String, T>>() {}.getType());
+        return map;
+    }
+
 }
