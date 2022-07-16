db94360d31e3d05fc8dc3560b2fcc1f98fb8d9e3
Emcikem <18968479985@163.com>
2022-07-16 14:51:56 +0800

    add module

---
+++ gateway-client/src/main/java/com/emcikem/gateway/response/BaseResponse.java
@@ -0,0 +1,9 @@
+package com.emcikem.gateway.response;
+
+/**
+ * @author Emcikem
+ * @create 2022/7/16
+ * @desc
+ */
+public class BaseResponse {
+}
---
+++ gateway-dao/src/main/java/com/emcikem/gateway/entity/UserDO.java
@@ -0,0 +1,9 @@
+package com.emcikem.gateway.entity;
+
+/**
+ * @author Emcikem
+ * @create 2022/7/16
+ * @desc
+ */
+public class UserDO {
+}
